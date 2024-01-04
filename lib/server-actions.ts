7cb3415d6efbc66287addf12b3b2c6969cd7f0c3
graemen16 <graemen16@gmail.com>
2024-01-04 13:26:39 +0000

    return subdomain text

--- lib/server-actions.ts
+++ lib/server-actions.ts
@@ -1,11 +1,16 @@
 'use server';
 import { headers } from 'next/headers';
+import subdomains from '@/subdomains.json';
 
 export async function sayHello() {
 	// find subdomain
 	const heads = headers();
-	const pathname = heads.get('next-url');
-	const referer = heads.get('referer');
+	//const pathname = heads.get('next-url');
+	//const referer = heads.get('referer');
+	const hostname = heads.get('host');
+	const subdomain = hostname!.split('.')[0];
+	const subdomainData = subdomains.find((d) => d.subdomain === subdomain);
+	const subdomainJson = JSON.stringify(subdomainData);
 
-	return 'Hello from server-only! ' + pathname + ', ' + referer;
+	return 'Hello from server-only! ' + subdomainJson;
 }
