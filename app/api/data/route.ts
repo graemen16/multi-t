81d0305e3657b1fc05108948f4810e9db28ecfac
graemen16 <graemen16@gmail.com>
2024-01-04 13:43:29 +0000

    clean up

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -3,17 +3,16 @@ import subdomains from '@/subdomains.json';
 
 export async function GET(request: Request) {
 	const url = new URL(request.url);
-	//const path = url.pathname;
 	let subdomain = url.hostname.split('.')[0];
+	// above is OK for prod, but for local dev we need to get the subdomain from the header
 	let hostname = request.headers.get('host');
-	//console.log('Route: subdomain', url, subdomain);
-	//console.log('Route: request.headers.get("host")', request.headers.get('host'));
-	const headerSubdomain = hostname?.split('.')[0];
-	//console.log('Route: headerSubdomain', headerSubdomain, 'subdomain', subdomain);
 	if (url.hostname === 'localhost' || url.hostname === 'localtest.me') {
-		subdomain = headerSubdomain!;
+		subdomain = hostname?.split('.')[0]!;
 	}
 	const subdomainData = subdomains.find((d) => d.subdomain === subdomain);
-	//console.log('Route: subdomainData', subdomainData);
+
+	if (!subdomainData) {
+		// no valid subdomain do something?
+	}
 	return new Response(JSON.stringify(`Hello from ${subdomain}!`), { status: 200 });
 }
