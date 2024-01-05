c039519ea62ba455cc6d8d710186c622a7310373
graemen16 <graemen16@gmail.com>
2024-01-05 11:37:01 +0000

    get subdomain from header

--- lib/server-actions.ts
+++ lib/server-actions.ts
@@ -5,12 +5,13 @@ import subdomains from '@/subdomains.json';
 export async function sayHello() {
 	// find subdomain
 	const heads = headers();
-	//const pathname = heads.get('next-url');
-	//const referer = heads.get('referer');
+	const pathname = heads.get('next-url');
+	const newDomain = pathname?.split('/')[2];
+	const referer = heads.get('referer');
 	const hostname = heads.get('host');
 	const subdomain = hostname!.split('.')[0];
 	const subdomainData = subdomains.find((d) => d.subdomain === subdomain);
-	const subdomainJson = JSON.stringify(subdomainData);
+	const subdomainJson = JSON.stringify({ ...subdomainData, pathname, newDomain, referer });
 
 	return 'Hello from server-only! ' + subdomainJson;
 }
